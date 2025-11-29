232d53c18d2e4d202d1f7e3eb191327ceeb0587a
yoru-no-mel <yoru.melancholia@proton.me>
2025-11-29 10:11:46 +0000

    init db migration

--- main.go
+++ main.go
@@ -39,39 +39,4 @@ func main() {
 	defer db.Close(dbConn)
 
 	db.AutoMigrate(config)
-
-	// server := api.NewServer(
-	// 	config,
-	// 	store,
-	// 	tsHandler,
-	// 	log,
-	// )
-
-	// Create a Gin router with default middleware (logger and recovery)
-	// r := gin.Default()
-
-	// r.GET("/", func(c *gin.Context) {
-	// 	// Return JSON response
-	// 	c.JSON(http.StatusOK, gin.H{
-	// 		"message": "DAMBASS",
-	// 	})
-	// })
-
-	// // Define a simple GET endpoint
-	// r.GET("/ping", func(c *gin.Context) {
-	// 	// Return JSON response
-	// 	c.JSON(http.StatusOK, gin.H{
-	// 		"message": "pong masa siiii",
-	// 	})
-	// })
-
-	// r.GET("/auth", func(c *gin.Context) {
-	// 	c.JSON(http.StatusUnauthorized, gin.H{
-	// 		"message": "auth missing hehe hoho",
-	// 	})
-	// })
-
-	// if err := r.Run(); err != nil {
-	// 	log.Fatalf("failed to run server: %v", err)
-	// }
 }
